c8bb680d8862d0c2fdd5803410327283bf3fb5df
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2024-09-09 19:22:45 +0200

    Update main.cpp

--- examples/platformio_basic/src/main.cpp
+++ examples/platformio_basic/src/main.cpp
@@ -40,13 +40,6 @@ void setup()
   // HERE THE HARDWARE SERIAL INTERFACE 1 IS USED
   ld2450.begin(Serial1, false);
 
-
-
-  if(!ld2450.waitForSensorMessage()){
-    Serial.println("SENSOR CONNECTION SEEMS OK");
-  }else{
-    Serial.println("SENSOR TEST: GOT NO VALID SENSORDATA - PLEASE CHECK CONNECTION!");
-  }
   
   Serial.println("SETUP_FINISHED");
   
